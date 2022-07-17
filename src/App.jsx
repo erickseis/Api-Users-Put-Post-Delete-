66aed47a3d524ac725c94a4c26a767500c7a7750
erickseis <erickseis@gmail.com>
2022-07-16 23:29:39 -0400

    sad

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import UserList from './UserList'
+import './App.css'
+import UsersForm from './UsersForm'
+
+function App() {
+
+  const [users, setUsers] = useState([])
+  const [userSelected, setUserSelected] = useState(null)
+  const [openModal, setOpenModal] = useState(false)
+  
+  useEffect(() => { //para llamar los datos que estan en la API
+    axios.get('https://users-crud1.herokuapp.com/users/')
+      .then(res => setUsers(res.data))
+  }, [])
+  console.log(users)
+
+  const getUser = () => {
+    axios.get('https://users-crud1.herokuapp.com/users/')
+      .then(res => setUsers(res.data))
+  }
+
+  const addUsers = newUser =>{
+    alert('añadiendo')
+    axios.post('https://users-crud1.herokuapp.com/users/', newUser)
+    .then(() => getUser())
+    .catch((error) =>console.log(error.response));
+
+  }
+  const deleteUser = (id) => {
+    axios.delete(`https://users-crud1.herokuapp.com/users/${id}/`)
+    .then(()=> getUser());
+  };
+
+  const selectUser = user => {
+    setUserSelected(user)
+  }
+
+  const deselectUser = () => setUserSelected(null)
+
+  const updateUser = (userUpdate) =>{
+    alert('Actualizando');
+    axios.put(`https://users-crud1.herokuapp.com/users/${userUpdate.id}/`, userUpdate)
+    .then(() => getUser())
+    .catch(error=>console.log(error.response))
+
+  }
+
+console.log(userSelected)
+
+
+  return (
+    <div className="App">
+
+      <button className='modalBtn' onClick={()=> setOpenModal(true)}>Create User</button>
+      <UsersForm 
+      userSelected={userSelected}
+      addUsers={addUsers}
+      selectUser={selectUser}
+      updateUser={updateUser}
+      deselectUser={deselectUser}
+      openModal={openModal}
+      onClose={()=> setOpenModal(false)}
+      />
+      
+      <UserList 
+        users={users} 
+        deleteUser={deleteUser}
+        selectUser={selectUser}
+
+        />
+        
+        
+    </div>
+  )
+}
+
+export default App
---
+++ src/UsersForm.jsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+const UsersForm = ({
+    addUsers,
+    selectUser,
+    updateUser,
+    deselectUser,
+    userSelected,
+    openModal,
+    onClose
+}) => {
+    if (!openModal) return null
+
+
+
+    const [firstName, setFirtsName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPasword] = useState("");
+    const [birthday, setBirthday] = useState("");
+
+    useEffect(() => {
+        if (userSelected !== null) {
+            setFirtsName(userSelected.first_name)
+            setLastName(userSelected.last_name)
+            setEmail(userSelected.email)
+            setPasword(userSelected.password)
+            setBirthday(userSelected.birthday)
+
+        } else {
+            reset();
+        }
+    }, [userSelected]);
+
+    const submit = (e) => {
+        e.preventDefault();
+        alert("hice submit")
+
+        const userForm = {
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            password: password,
+            birthday: birthday
+        };
+
+
+        if (userSelected !== null) {
+            userForm.id = userSelected?.id
+            updateUser(userForm);
+            deselectUser();
+        } else {
+            addUsers(userForm);
+            reset();
+
+        }
+    }
+
+    const reset = () => {
+        setFirtsName("")
+        setLastName("")
+        setEmail("")
+        setPasword("")
+        setBirthday("")
+
+    }
+const clear =()=>{
+    reset();
+}
+
+    return (
+
+        <form onSubmit={submit}>
+            <div className='container-form'>
+                <p onClick={onClose} className="closeBtn">X</p>
+
+                <div className='input-container'>
+                    <label htmlFor="firstName">First Name</label>
+                    <br />
+                    <input type="text"
+                        id='firstName'
+                        onChange={(e) => setFirtsName(e.target.value)}
+                        value={firstName}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="lastName">Last Name</label>
+                    <br />
+                    <input type="text"
+                        id='lastName'
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <br />
+                    <input type="text"
+                        id='email'
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <br />
+                    <input type="password"
+                        id='password'
+                        onChange={(e) => setPasword(e.target.value)}
+                        value={password}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="birthday">Birthday</label>
+                    <br />
+                    <input type="date"
+                        id='birthday'
+                        onChange={(e) => setBirthday(e.target.value)}
+                        value={birthday}
+                    />
+                </div>
+
+                <button className='created '>{userSelected !== null ? "Update" : "Create"}</button>
+                {userSelected !== null && (
+                    <button className='clean' onClick={clear} type="button">Clear
+                    </button>
+                )}
+            </div>
+        </form>
+
+    );
+
+};
+export default UsersForm;
